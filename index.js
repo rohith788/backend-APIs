1afdd128d77a3a27890951b911b56a4a847b5564
srohith788 <srohith788@gmail.com>
2022-11-01 01:13:06 -0500

    tests added

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ var time_date = (date_time) => {
     return split_data
 }
 
+
+var addNegPoints = (points, payer) => {//function to add a negative points tranaction
+    
+    var pvePoitns = Math.abs(points) // getting the absolute points to avoid confucion during later steps
+    var indexes = [] // using array to store indexes becase we cannot delete the element form the array furing the transaction
+    var c = transactions.length - 1
+    console.log('negative')
+    while (pvePoitns > 0){
+        var payerForThisTransaction = transactions[c]['payer']
+        var pointsForThisTransaction = transactions[c]['points']
+        if (payerForThisTransaction === payer ){//searching for the payer
+        if(pvePoitns === pointsForThisTransaction){//condition for tranaction points equal to point we need to remove
+                pvePoitns = 0// the points have been used up
+                indexes.push(c)//adding index to delete
+            }
+            else if(pvePoitns < pointsForThisTransaction){
+                transactions[c]['points'] -= pvePoitns
+                pvePoitns = 0
+            }
+            else{
+                pvePoitns -= pointsForThisTransaction
+                indexes.push(c)
+            }
+        }
+        c--
+    }
+    for (let i = indexes.length - 1; i >= 0; i--){
+        transactions.splice(i,1)
+    }
+
+}
+
 app.post('/addPoints', 
     (req, res) => 
     {   //storing the data in variables
@@ -24,30 +56,36 @@ app.post('/addPoints',
         var points = req.body.points
         var transactionTime = req.body.timestamp
         
-        //checking in the buyer already exists
-        if (payer in pointsPerPayer) {
-            // checking if the points are going into -ve
-            if (pointsPerPayer[payer] + points < 0){return res.status(400).json({ error: "Unable to add transaction", reason: "Payer Points is turing Negative"})}
-            else { 
-                pointsPerPayer[payer] += points 
-                totalPoints += points
-                transactions.push({'payer': payer, 'points': points, 'time': transactionTime})
-                transactions = transactions.sort((a, b) => {
-                    return (a.time < b.time) ? -1 : ((a.time > b.time) ? 1 : 0);
-                })
+        if (points === 0) res.status(400).json({ error: "Unable to add transaction", reason: "Points is 0"})
+        
+        else if (points < 0) addNegPoints(points, payer)
+
+        else{
+            //checking in the buyer already exists
+            if (payer in pointsPerPayer) {
+                // checking if the points are going into -ve
+                if (pointsPerPayer[payer] + points < 0){return res.status(400).json({ error: "Unable to add transaction", reason: "Payer Points is turing Negative"})}
+                else { 
+                    pointsPerPayer[payer] += points 
+                    totalPoints += points
+                    transactions.push({'payer': payer, 'points': points, 'time': transactionTime})
+                    transactions = transactions.sort((a, b) => {
+                        return (a.time < b.time) ? -1 : ((a.time > b.time) ? 1 : 0);
+                    })
+                    }
+            }//if buyer dosnt exist
+            else {
+                // checking if the points are going into -ve
+                if (points >= 0) {
+                    pointsPerPayer[payer] = points
+                    totalPoints += points
+                    transactions.push({'payer': payer, 'points': points, 'time': transactionTime})
+                    transactions = transactions.sort((a, b) => {
+                        return (a.time < b.time) ? -1 : ((a.time > b.time) ? 1 : 0);
+                    })
                 }
-        }//if buyer dosnt exist
-        else {
-            // checking if the points are going into -ve
-            if (points >= 0) {
-                pointsPerPayer[payer] = points
-                totalPoints += points
-                transactions.push({'payer': payer, 'points': points, 'time': transactionTime})
-                transactions = transactions.sort((a, b) => {
-                    return (a.time < b.time) ? -1 : ((a.time > b.time) ? 1 : 0);
-                })
+                else return res.status(400).json({ error: "Unable to add transaction", reason: "User has no payer points to spend"})
             }
-            else return res.status(400).json({ error: "Unable to add transaction", reason: "User has no payer points to spend"})
         }
         console.log(transactions)
         res.status(200).json({ success: "success", transaction: req.body})
@@ -55,44 +93,49 @@ app.post('/addPoints',
 )
 
 app.post('/spendPoints', (req, res) => {
-    var pointsToSpend = parseInt(req.body.points)
+    if(transactions.length === 0) res.status(400).json({error: "no points to spend"})
+    var pointsToSpend = parseInt(req.body.points) // getiing the input
+    //error messages for edge cases
     if (pointsToSpend <= 0) res.status(400).json({ error: "Unable to perform transaction", reason: "there are no points or is -ve"})
     if (pointsToSpend > totalPoints) res.status(400).json({ error: "Unable to perform transaction", reason: "user does not have enough points"})
     var points_spent = {}
     var counter = 0
     var indexes = []
+    //loop until all the points are spent
     while (pointsToSpend > 0){
         var payer = transactions[counter]['payer']
         var points = transactions[counter]['points']
-        if (points === pointsToSpend){
-            if (payer in points_spent) points_spent[payer] += pointsToSpend
+        if (points === pointsToSpend){//condition in the points in that transaction is perfectly equal to the number of points we need to spend
+            if (payer in points_spent) points_spent[payer] += pointsToSpend//update the payer/points spent
             else points_spent[payer] = pointsToSpend
-            pointsPerPayer[payer] -= pointsToSpend
-            pointsToSpend = 0
-            indexes.push(counter)
+            pointsPerPayer[payer] -= pointsToSpend//upating the spent points
+            pointsToSpend = 0//all the points have been assigned
+            indexes.push(counter)//storing index to delete the element later
         }
-        else if (points > pointsToSpend){
-            if (payer in points_spent) points_spent[payer] += pointsToSpend
+        else if (points > pointsToSpend){//condition if the tranaction has more points than we need to spend
+            if (payer in points_spent) points_spent[payer] += pointsToSpend//update the payer/points spent
             else points_spent[payer] = pointsToSpend
-            transactions[counter]['points'] -= pointsToSpend
-            pointsPerPayer[payer] -= pointsToSpend
-            pointsToSpend = 0
+            transactions[counter]['points'] -= pointsToSpend//updating the points for the transaction
+            pointsPerPayer[payer] -= pointsToSpend//upating the spent points
+            pointsToSpend = 0//all the points have been assigned
         }
-        else{
-            console.log(pointsToSpend)
-            points_spent[payer] = points
-            pointsToSpend -= points
-            pointsPerPayer[payer] -= points
-            indexes.push(counter)
+        else{//condition where the points for the tranaction is less then the needed point 
+            points_spent[payer] = points//update the payer/points spent
+            pointsToSpend -= points//upadtin the points to see how much more we have to spend
+            pointsPerPayer[payer] -= points //upating the spent points
+            indexes.push(counter)//storing index to delete the element later
         }
         counter++
     }
-    totalPoints -= pointsToSpend
-    for( let i = indexes.length - 1; i >= 0; i--){
+    totalPoints -= pointsToSpend // updating the total points left for the user 
+    for( let i = indexes.length - 1; i >= 0; i--){//loop to delete the tranactions used 
         transactions.splice(i, 1)
     }
-    console.log(points_spent)
-    res.status(200).json(points_spent)
+    var points_spent_array = []
+    for( const key in points_spent){
+        points_spent_array.push( {'payer' : key , 'points' : (points_spent[key] * (-1))})
+    }
+    res.status(200).json(points_spent_array)
 })
 
 app.get('/', (req, res) => res.send(pointsPerPayer)) // display the total points remaining for the user
---
+++ test/test.js
@@ -0,0 +1,138 @@
+const server  =  require('../index')
+const API = 'http://localhost:5000'
+const { expect } = require('chai')
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+
+chai.should()
+
+chai.use(chaiHTTP)
+
+describe('Points API',  () => {
+
+  // POST call for addPoints
+  describe(" add points transactions", () => {
+    it("This should succeed ", (done) => {
+      const transaction = {"payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z"}
+      chai.request(API)
+          .post("/addPoints")
+          .send(transaction)
+          .end((err, res) => {
+            res.should.have.status(200)
+            expect(res.body.transaction).to.deep.equal(transaction)
+          done()
+          })
+    })
+    
+    describe(" add points transactions", () => {
+        it("This should succeed ", (done) => {
+          const transaction = { "payer": "UNILEVER", "points": 200, "timestamp": "2022-10-31T11:00:00Z" }
+          chai.request(API)
+              .post("/addPoints")
+              .send(transaction)
+              .end((err, res) => {
+                res.should.have.status(200)
+                expect(res.body.transaction).to.deep.equal(transaction)
+              done()
+              })
+        })
+    })
+    describe(" add points transactions", () => {
+        it("This should succeed ", (done) => {
+            const transaction = { "payer": "DANNON", "points": -200, "timestamp": "2022-10-31T15:00:00Z" }
+            chai.request(API)
+                .post("/addPoints")
+                .send(transaction)
+                .end((err, res) => {
+                res.should.have.status(200)
+                expect(res.body.transaction).to.deep.equal(transaction)
+                done()
+                })
+        })
+    })
+    describe(" add points transactions", () => {
+        it("This should succeed ", (done) => {
+            const transaction = { "payer": "MILLER COORS", "points": 10000, "timestamp": "2022-11-01T14:00:00Z" }
+            chai.request(API)
+                .post("/addPoints")
+                .send(transaction)
+                .end((err, res) => {
+                res.should.have.status(200)
+                expect(res.body.transaction).to.deep.equal(transaction)
+                done()
+                })
+        })
+    })
+    describe(" add points transactions", () => {
+        it("This should succeed ", (done) => {
+            const transaction = { "payer": "DANNON", "points": 1000, "timestamp": "2022-11-02T14:00:00Z" }
+            chai.request(API)
+                .post("/addPoints")
+                .send(transaction)
+                .end((err, res) => {
+                res.should.have.status(200)
+                expect(res.body.transaction).to.deep.equal(transaction)
+                done()
+                })
+        })
+    })
+
+    it("This should fail", (done) => {
+      const transaction = {"p": "DANNON", "p": 300, "t": "2020-10-31T10:00:00Z", "extra": "extra"}
+      chai.request(API)
+          .post("/addPoints")
+          .send(transaction)
+          .end((err, res) => {
+            res.should.have.status(400)
+          done()
+          })
+    })
+    
+  })
+
+  
+  //POST call to spend the points
+  describe("Spend user points ", () => {
+      it("Should spend points available", (done) => {
+          const spend = {"points":  5000}
+          const response  = [
+            { "payer": "DANNON", "points": -100 },
+            { "payer": "UNILEVER", "points": -200 },
+            { "payer": "MILLER COORS", "points": -4700 }
+            ]
+            
+          chai.request(API)
+          .post("/spendPoints")
+          .send(spend)
+          .end((err, res) => {
+              res.should.have.status(200)
+              expect(res.body).to.deep.equal(response)
+              done()
+            })
+        })
+    })
+
+    // GET call to get current points
+    describe("Get current points", () => {
+      it("Should display all the points by payer", (done) => {
+        const points = {"DANNON": 1000,
+            "UNILEVER" : 0,
+            "MILLER COORS": 5300}            
+        chai.request(API)
+          .get("/")
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            expect(res.body).to.deep.equal(points)
+          done()
+          })
+      })
+    })
+
+})
+
+
+
+
+
+
